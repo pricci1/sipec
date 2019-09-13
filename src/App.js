fdd63f4a643c6c302301fd2f3f95b0895fd764d8
Pedro <priccis@gmail.com>
2019-09-13 19:39:37 -0300

    feat(sidebar): add static sidebar
    For now it's just the stucture, it doesn't know wich page
    is active for now

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Link } from "@reach/router";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
 
 const App = () => {
   return (
@@ -13,7 +14,9 @@ const App = () => {
       </header>
       <div className="container-fluid">
         <div className="row">
-          <div className="col-2 bg-info">Sidebar here</div>
+          <div className="col-2">
+            <Sidebar />
+          </div>
           <main className="col-10 bg-light">
             Main content here <h2>Que?</h2>
           </main>
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Link } from "@reach/router";
+import "./sidebar.css";
+
+const Sidebar = () => {
+  const [visible, setVisible] = useState(true);
+
+  return (
+    <nav
+      className="bg-light sidebar"
+      style={{ width: visible ? "100%" : "5%" }}
+    >
+      <div className="sidebar-sticky">
+        <ul className="nav flex-column">
+          <li className="nav-item">
+            <Link className="nav-link active" to="#">
+              Administración DIIOs
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              Administración animal con DIIO
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              Establecimiento
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              Existencias animales
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              Movimientos animales
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Sidebar;
