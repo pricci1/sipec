9f18381009e43e3e61b2eb1b1760c35dbe360972
ignaciofigueroa <ijfigueroa@gmail.com>
2019-09-26 15:13:56 -0300

    feat(diio): -Add base files -Add .idea/ to gitignore

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { Link } from "@reach/router";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import from "./Components"
 const App = () => {
   return (
     <div>
@@ -14,9 +14,7 @@ const App = () => {
       <div className="container-fluid">
         <div className="row">
           <div className="col-2 bg-info">Sidebar here</div>
-          <main className="col-10 bg-light">
-            Main content here <h2>Que?</h2>
-          </main>
+          
         </div>
       </div>
     </div>
---
+++ src/components/Diio/NewPucharseDiio.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+function NewPucharseDiio(props){
+    return(
+        <h2>Nueva Compra</h2>
+        <br></br>
+    )
+}
