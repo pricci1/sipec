e7ab9b1e811892377422094d3f970fcc717aefb4
Pedro <priccis@gmail.com>
2019-09-27 23:18:51 -0300

    feat(api): api provider
    Using the Context API,  provide an instance of the API class which manag-
    es the token

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import ReactDOM from "react-dom";
 import { Link } from "@reach/router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./components/User/Login";
+import APIProvider from "./components/APIProvider";
 
 const App = () => {
   return (
-    <div>
-      <header className="bg-dark">
-        <Link to="/">SIPEC</Link>
-        <br />
-        <span>Navbar here</span>
-      </header>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-2 bg-info">Sidebar here</div>
-          <main className="col-10 bg-light">
-            Main content here <h2>Que?</h2>
-            <Login></Login>
-          </main>
+    <APIProvider>
+      <div>
+        <header className="bg-dark">
+          <Link to="/">SIPEC</Link>
+          <br />
+          <span>Navbar here</span>
+        </header>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-2 bg-info">Sidebar here</div>
+            <main className="col-10 bg-light">
+              Main content here <h2>Que?</h2>
+              <Login></Login>
+            </main>
+          </div>
         </div>
       </div>
-    </div>
+    </APIProvider>
   );
 };
 
