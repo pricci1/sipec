3e007a0a3f3f8928d27efb66f75ff01d6ee96221
ignaciofigueroa <ijfigueroa@gmail.com>
2019-09-29 10:51:27 -0300

    feat(newPurchaseDiio): -Implemented form structure -Not yet conected

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, {  } from "react";
 import ReactDOM from "react-dom";
 import { Link } from "@reach/router";
 import "bootstrap/dist/css/bootstrap.min.css";
-import from "./Components"
+import NewPucharseDiio from "./components/Diio/NewPucharseDiio"
 const App = () => {
   return (
     <div>
@@ -14,7 +14,7 @@ const App = () => {
       <div className="container-fluid">
         <div className="row">
           <div className="col-2 bg-info">Sidebar here</div>
-          
+          <NewPucharseDiio/>
         </div>
       </div>
     </div>
