c3fe350445bae6c792894207f8aecfee9db7402c
beannama <ben.nar.95@gmail.com>
2019-10-01 01:24:38 -0300

    Table UPDATED

--- src/components/AnimalMoves/SIPECtable.js
+++ src/components/AnimalMoves/SIPECtable.js
@@ -3,7 +3,79 @@ import ReactTable from "react-table";
 import "react-table/react-table.css";
 
 const AnimalMovesTable = props => {
-  const { headers, data } = props;
+  const { data,cases } = props;
+
+  if(cases == 1){
+    const headers=[{
+      Header:'ID Registro',
+      acessor: 'idregistro'
+  },{
+      Header:'RUP',
+      acessor: 'rup'
+  },{
+    Header:'Establecimiento',
+    acessor: 'establecimiento'
+  },{
+    Header:'Titular',
+    acessor: 'titular'
+  },{
+    Header:'Fecha Registro',
+    acessor: 'fecharegistro'
+  },{
+    Header:'Cantidad',
+    acessor: 'cantidad'
+  }];
+  }else if (cases==2){
+    const headers=[{
+      Header:'ID Registro',
+      acessor: 'idregistro'
+  },{
+      Header:'Vendedor',
+      acessor: 'vendedor'
+  },{
+    Header:'Comprador',
+    acessor: 'comprador'
+  },{
+    Header:'Fecha',
+    acessor: 'fecha'
+  },{
+    Header:'Estado',
+    acessor: 'estado'
+  },{
+    Header:'RUT-Comprador',
+    acessor: 'rutcomprador'
+  }];
+  }else if (cases==3){
+    const headers=[{
+      Header:'DIIO',
+      acessor: 'diio'
+  },{
+      Header:'Especie',
+      acessor: 'especie'
+  },{
+    Header:'Marca DIIO',
+    acessor: 'marcadiio'
+  },{
+    Header:'Rut Comprador',
+    acessor: 'rutcomprador'
+  },{
+    Header:'Raza',
+    acessor: 'raza'
+  },{
+    Header:'Sexo',
+    acessor: 'sexo'
+  },{
+    Header:'Fecha Nacimiento',
+    acessor: 'fechanacimiento'
+  },{
+    Header:'Categoria',
+    acessor: 'categoria'
+  },{
+    Header:'Agregar Animal',
+    acessor: 'agregaranimal'
+  }];
+  }
+  
 
   return <ReactTable data={data} columns={headers} />;
   /*(
