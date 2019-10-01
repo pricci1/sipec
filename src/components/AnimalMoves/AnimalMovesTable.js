9dfd750562882119854b75e6586e13585ea7e819
johnny <jadonoso2@miuandes.cl>
2019-10-01 17:46:11 -0300

    add new table , falta modal

--- src/components/AnimalMoves/AnimalMovesTable.js
+++ src/components/AnimalMoves/AnimalMovesTable.js
@@ -1,31 +1,54 @@
 import React from "react";
+import { MDBDataTable } from 'mdbreact';
+import ModalAnimalMoves from "./ModalAnimalMoves";
 
 const AnimalMovesTable = props => {
   const { headers, data } = props;
 
+  let columns = [];
+  let rows = [];
+
+  headers.map(header => (
+    columns.push({
+      label: header, 
+      field: header.toLowerCase(), 
+      sort: 'asc',
+      width: 200})
+  ));
+
+  function getColumnItem(index, item) {
+    if(index == 0){
+      return <ModalAnimalMoves text={item[index]}/>
+    } else {
+      return item[index]
+    }
+  }
+
+  var jsonRow = {};
+  data.map(item => (
+    columns.map((col, index) => (
+      jsonRow[String(col.field)] = getColumnItem(index, item)
+    )),
+    rows.push(jsonRow),
+    jsonRow = {}
+  ));
+
+  const finalData = {columns, rows};
+  
   return (
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          {headers.map(header => (
-            <th key={header.replace(" ", "-")} scope="col">
-              {header}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index + 1}>
-            <th scope="row">{index + 1}</th>
-            {item.map(el => (
-              <td key={el.replace(" ", "-")}>{el}</td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <MDBDataTable
+      striped
+      scrollY
+      hover
+      bordered
+      small
+      maxHeight="370px"
+      data={finalData}
+      entriesLabel={["Mostrar entradas"]}
+      infoLabel={["Mostrando de", "a", "entradas, de"]}
+      paginationLabel={["Anterior", "Siguiente"]}
+      searchLabel={["Buscar"]}
+    />
   );
 };
 
---
+++ src/components/AnimalMoves/ModalAnimalMoves.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const PucharseDetailsDiioModal = props => {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    return (
+      <>
+        <a style={{color: 'blue'}} onClick={handleShow} href="/">{props.text}</a>
+        <div className="modal" show={show} onHide={handleClose}>
+          <div className="modal-header" closeButton>
+            <h4 className="modal-title">Modal heading</h4>
+          </div>
+          <div className="modal-body">{props.text}</div>
+          <div className="modal-footer">
+            <div role="button" className="btn btn-primary" onClick={handleClose}>
+              Close
+            </div>
+            <div role="button" className="btn btn-secondary" onClick={handleClose}>
+              Save Changes
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default PucharseDetailsDiioModal;
