0ee1603f398585aed78f8fa730d09ee6fbef2556
Vicente Correa <vicorrea@miuandes.cl>
2019-10-01 00:58:37 -0300

    Change components names (equal to file name) and change react bootstrap to normal bootstrap (in modal)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Link } from "@reach/router";
 import "bootstrap/dist/css/bootstrap.min.css";
-import DiioTable from "./components/Diio/PucharseListDiio";
+import PucharseListDiio from "./components/Diio/PucharseListDiio";
 
 const App = () => {
   return (
@@ -15,7 +15,7 @@ const App = () => {
       <div className="container-fluid">
         <div className="row">
           <div className="col-2 bg-info">Sidebar here</div>
-            <DiioTable headers={[/*Aquí van los headers de la tabla (como en el playpen)*/]} data={[/*Aquí va la data (como en el playpen)*/]}/>
+            <   PucharseListDiio headers={[/*Aquí van los headers de la tabla (como en el playpen)*/]} data={[/*Aquí va la data (como en el playpen)*/]}/>
         </div>
       </div>
     </div>
