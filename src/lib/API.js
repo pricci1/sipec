50f0fb20f90519c818f85d53af95db147f9caf90
Pedro <priccis@gmail.com>
2019-09-30 23:52:06 -0300

    feat(api):  get and post with correct token

--- src/lib/API.js
+++ src/lib/API.js
@@ -4,7 +4,9 @@ class API {
   constructor() {
     // check if token in storage and copy it to token
     this.token = localStorage.token || null;
-    this.apiUrl = "https://sipec-backend.herokuapp.com/";
+    this.client = localStorage.client || null;
+    this.uid = localStorage.uid || null;
+    this.apiUrl = "https://sipec-backend.herokuapp.com";
   }
 
   login = async (email, password) => {
@@ -34,30 +36,48 @@ class API {
   recoverAccount;
 
   get = async url => {
-    await axios
-      .get(url, {
+    const path = this.apiUrl + url;
+    var results = { success: false, data: null };
+    try {
+      const getResponse = await axios.get(path, {
         headers: {
           "Content-Type": "application/json",
-          Authorization: `bearer ${this.token}`
+          "access-token": this.token,
+          client: this.client,
+          uid: this.uid
         }
-      })
-      .then(({ data }) => {
-        return data;
       });
+      if (getResponse.status === 200) {
+        results.success = true;
+        results.data = getResponse.data;
+      } else {
+        results.success = false;
+      }
+    } catch (error) {
+      results.success = false;
+      results.data = error;
+    }
+    // TODO: If the response says that the token is not valid, redirect to login
+    return results;
   };
 
-  post = (url, obj) => {
-    axios
-      .get(url, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `bearer ${this.token}`
-        },
-        ...obj
-      })
-      .then(({ data }) => {
-        return data;
-      });
+  post = async (url, obj) => {
+    const path = this.apiUrl + url;
+    var results = { success: false };
+    const postResponse = await axios.get(path, {
+      headers: {
+        "Content-Type": "application/json",
+        "access-token": this.token,
+        client: this.client,
+        uid: this.uid
+      },
+      ...obj
+    });
+    if (Math.floor(postResponse.status / 100) === 2) {
+      results.success = true;
+    }
+    // TODO: If the response says that the token is not valid, redirect to login
+    return results;
   };
 }
 
