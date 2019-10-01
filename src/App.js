3c7c85a187e7f2d10d5c3c79da3e651afedda0d4
beannama <ben.nar.95@gmail.com>
2019-10-01 18:05:41 -0300

    all in 1 page

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
               <Sidebar />
             </div>
             <main className="col-10">
-              {/* <ListadoBajaDIIO /> */}
+              <ListadoBajaDIIO />
               <StockDIIOEstablecimiento />
             </main>
           </div>
