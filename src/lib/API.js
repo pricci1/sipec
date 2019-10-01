e42b2483f5217249a3c8246b7112c86a07714a71
Pedro <priccis@gmail.com>
2019-10-01 18:04:26 -0300

    feat(api): set logged user id

--- src/lib/API.js
+++ src/lib/API.js
@@ -6,16 +6,20 @@ class API {
     this.token = localStorage.token || null;
     this.client = localStorage.client || null;
     this.uid = localStorage.uid || null;
+    this.currentUserId = localStorage.currentUserId || null;
     this.apiUrl = "https://sipec-backend.herokuapp.com";
   }
 
   login = async (email, password) => {
     var response = { success: false };
     await axios
-      .post(this.apiUrl + "auth/sign_in", { email, password })
+      .post(this.apiUrl + "/auth/sign_in", { email, password })
       .then(resp => {
         if (resp.status === 200) {
           this.token = resp.headers["access-token"];
+          this.client = resp.headers["client"];
+          this.uid = resp.headers["uid"];
+          this.currentUserId = resp.data.data["id"];
           response.success = true;
         } else {
           response.success = false;
@@ -24,6 +28,9 @@ class API {
       })
       .catch(error => console.log(error));
     localStorage.token = this.token;
+    localStorage.client = this.client;
+    localStorage.uid = this.uid;
+    localStorage.currentUserId = this.currentUserId;
 
     return response;
   };
@@ -64,18 +71,18 @@ class API {
   post = async (url, obj) => {
     const path = this.apiUrl + url;
     var results = { success: false };
-    const postResponse = await axios.post(path, {
-      headers: {
-        "Content-Type": "application/json",
-        "access-token": this.token,
-        client: this.client,
-        uid: this.uid
-      },
-      ...obj
-    });
-    if (Math.floor(postResponse.status / 100) === 2) {
-      results.success = true;
-    }
+      const postResponse = await axios.post(path, {
+        headers: {
+          "Content-Type": "application/json",
+          "access-token": this.token,
+          client: this.client,
+          uid: this.uid
+        },
+        ...obj
+      });
+      if (Math.floor(postResponse.status / 100) === 2) {
+        results.success = true;
+      }
     // TODO: If the response says that the token is not valid, redirect to login
     return results;
   };
