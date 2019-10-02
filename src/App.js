7a528e44207d7c4f6a37b007afb198e700b852f8
ignaciofigueroa <ijfigueroa@gmail.com>
2019-10-02 04:24:07 -0300

    reformat(feature-diio): -Merge branches -Style completing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import Login from "./components/User/Login";
 // import NewPurchaseDiio from "./components/Diio/NewPucharseDiio"
-import NotAppliedDroppedDiio from "./components/Diio/NotAppliedDroppedDiio";
+
 import { APIProvider } from "./components/APIProvider";
 
-import ListadoBajaDIIO from "./pages/listadoBajaDIIO";
-import StockDIIOEstablecimiento from "./pages/stockDIIOEstablecimiento";
 
 // import SIPECTable from "./components/AnimalMoves/SIPECtable";
 
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar/Sidebar";
 import Sipec from "./routes/Sipec";
 
 const App = () => {
