30688039d218b5709a426efdeaf9200266d56fd3
Vicente Correa <vicorrea@miuandes.cl>
2019-10-01 23:55:53 -0300

    Diio table and modal

--- src/components/Diio/PucharseDetailsDiioModal.js
+++ src/components/Diio/PucharseDetailsDiioModal.js
@@ -1,30 +1,105 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
 const PucharseDetailsDiioModal = props => {
+  const backendUrl = "http://sipec-backend.herokuapp.com";
 
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
-    return (
-      <>
-        <a style={{color: 'blue'}} onClick={handleShow} href="/">{props.text}</a>
-        <div className="modal" show={show} onHide={handleClose}>
-          <div className="modal-header" closeButton>
-            <h4 className="modal-title">Modal heading</h4>
-          </div>
-          <div className="modal-body">{props.text}</div>
-          <div className="modal-footer">
-            <div type="button" className="btn btn-primary" onClick={handleClose}>
-              Close
-            </div>
-            <div type="button" className="btn btn-secondary" onClick={handleClose}>
-              Save Changes
-            </div>
-          </div>
-        </div>
-      </>
+  const [details, setDetails] = useState([]);
+
+  async function getPurchaseDiioDetails() {
+    const detail = await axios.get(
+      `${backendUrl}/diio_purchases/${props.purchase_diio_id}`
     );
+    var {
+      diio_purchase: { confirmed, brand, created_at: date },
+      establishment: {
+        rup: purchaser_rut,
+        name: purchaser_name,
+        establishment_type: purchaser_type
+      },
+      diio_provider: { rut: provider_rut, person_type: provider_type }
+    } = detail.data;
+    setDetails({
+      confirmed,
+      brand,
+      date,
+      purchaser_name,
+      purchaser_rut,
+      purchaser_type,
+      provider_rut,
+      provider_type
+    });
   }
-  
-  export default PucharseDetailsDiioModal;
+
+  useEffect(() => {
+    getPurchaseDiioDetails();
+  }, []);
+
+  function getStringState(state) {
+    if (state) {
+      return "Vendido";
+    } else {
+      return "Espera";
+    }
+  }
+
+  return ReactDOM.createPortal(
+    <div
+      style={{
+        position: "absolute",
+        top: "0",
+        bottom: "0",
+        left: "0",
+        right: "0",
+        display: "grid",
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: "9999"
+      }}
+    >
+      <div
+        style={{
+          padding: 20,
+          background: "#fff",
+          borderRadius: "2px",
+          display: "inline-block",
+          minHeight: "50vh",
+          margin: "1rem",
+          position: "relative",
+          minWidth: "30vw",
+          boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
+          justifySelf: "center"
+        }}
+      >
+        <h2>Detalles compra</h2>
+        <hr></hr>
+        <p>
+          <b>Marca:</b> {details.brand}
+        </p>
+        <p>
+          <b>Estado:</b> {getStringState(details.confirmed)}
+        </p>
+        <h4>Datos del Vendedor</h4>
+        <p>
+          <b>Rol:</b> {details.provider_type}
+        </p>
+        <p>
+          <b>RUT:</b> {details.provider_rut}
+        </p>
+        <h4>Datos del Comprador</h4>
+        <p>
+          <b>Tipo:</b> {details.purchaser_type}
+        </p>
+        <p>
+          <b>RUT:</b> {details.provider_rut}
+        </p>
+        <p></p>
+        <button onClick={props.onClose}>Cerrar</button>
+      </div>
+    </div>,
+    document.querySelector("#modal")
+  );
+};
+
+export default PucharseDetailsDiioModal;
--- src/components/Diio/PucharseListDiio.js
+++ src/components/Diio/PucharseListDiio.js
@@ -1,55 +1,82 @@
-import React from "react";
-import { MDBDataTable } from 'mdbreact';
+import React, { useState } from "react";
+import { MDBDataTable } from "mdbreact";
 import PucharseDetailsDiioModal from "./PucharseDetailsDiioModal";
 
-const PucharseListDiio = props => {
+const DiioTable = props => {
   const { headers, data } = props;
+
+  const [show, setShow] = useState(false);
+  const [purchaseDiioId, setPurchaseDiioId] = useState(0);
+
+  const handleShowMessageClick = () => setShow(true);
+  const handleCloseModal = () => setShow(false);
+
+  const handleModal = props => {
+    handleShowMessageClick();
+    setPurchaseDiioId(props);
+  };
 
   let columns = [];
   let rows = [];
 
-  headers.map(header => (
+  headers.map(header =>
     columns.push({
-      label: header, 
-      field: header.toLowerCase(), 
-      sort: 'asc',
-      width: 200})
-  ));
+      label: header,
+      field: header.toLowerCase(),
+      sort: "asc",
+      width: 200
+    })
+  );
 
   function getColumnItem(index, item) {
-    if(index == 0){
-      return <PucharseDetailsDiioModal text={item[index]}/>
+    if (index == 0) {
+      return (
+        <a style={{ color: "blue" }} onClick={() => handleModal(item[index])}>
+          {item[index]}
+        </a>
+      );
     } else {
-      return item[index]
+      return item[index];
     }
   }
 
   var jsonRow = {};
-  data.map(item => (
-    columns.map((col, index) => (
-      jsonRow[String(col.field)] = getColumnItem(index, item)
-    )),
-    rows.push(jsonRow),
-    jsonRow = {}
-  ));
+  data.map(
+    item => (
+      columns.map(
+        (col, index) =>
+          (jsonRow[String(col.field)] = getColumnItem(index, item))
+      ),
+      rows.push(jsonRow),
+      (jsonRow = {})
+    )
+  );
 
-  const finalData = {columns, rows};
-  
+  const finalData = { columns, rows };
+
   return (
-    <MDBDataTable
-      striped
-      scrollY
-      hover
-      bordered
-      small
-      maxHeight="370px"
-      data={finalData}
-      entriesLabel={["Mostrar entradas"]}
-      infoLabel={["Mostrando de", "a", "entradas, de"]}
-      paginationLabel={["Anterior", "Siguiente"]}
-      searchLabel={["Buscar"]}
-    />
+    <>
+      <MDBDataTable
+        striped
+        scrollY
+        hover
+        bordered
+        small
+        maxHeight="370px"
+        data={finalData}
+        entriesLabel={["Mostrar entradas"]}
+        infoLabel={["Mostrando de", "a", "entradas, de"]}
+        paginationLabel={["Anterior", "Siguiente"]}
+        searchLabel={["Buscar"]}
+      />
+      {show ? (
+        <PucharseDetailsDiioModal
+          onClose={handleCloseModal}
+          purchase_diio_id={purchaseDiioId}
+        />
+      ) : null}
+    </>
   );
 };
 
-export default PucharseListDiio;
+export default DiioTable;
