a14588eab37dd99313badd774072f8bc5cb9dd29
beannama <ben.nar.95@gmail.com>
2019-10-28 18:57:20 -0300

    added table on muerte

---
+++ src/components/AnimalAdministration/SearchAnimalDownDIIO.js
@@ -0,0 +1,117 @@
+import React, { useContext, useState } from "react";
+import { Formik } from "formik";
+import { Datepicker } from "react-formik-ui";
+
+import useModal from "../Modal";
+
+// import { getSpecies, getBrands, getModels } from "../lib/APIDiio";
+import APIContext from "../APIProvider";
+
+import Selector from "../Diio/Utilities/FormikSelector";
+import AnimalDeathRegistrationTab from "../../routes/AnimalAdministrationMenuTabs/AnimalDeathRegistrationTab";
+import { AnimalEstablishmentRegistryTable } from "./AnimalEstablishmentRegistryTable";
+import AnimalEstablishmentRecordDetails from "./AnimalEstablishmentRecordDetails";
+
+const SearchAnimalDownDIIO = () => {
+  const api = useContext(APIContext);
+  const { modal: Modal, modalIsOpened, toggleModal } = useModal();
+  const [modalRegistryId, setModalRegistryId] = useState();
+  const [data, setData] = useState([]);
+
+  // async function getSpeciesAPI() {
+  //   const data = await getSpecies(api);
+  //   console.log(data);
+  //   return data;
+
+  async function getEstablishments() {
+    return [
+      { value: 1, label: "El Salto de Pilmaiquen" },
+      { value: 2, label: "La Mosqueta" }
+    ];
+  }
+
+  return (
+    <div className="body">
+      <h2>Buscar baja animales con DIIO</h2>
+      <div>
+        <Formik
+          initialValues={{
+            establishment_id: "",
+            desde: "",
+            hasta: ""
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+            //console.log(values);
+          }}
+        >
+          {({
+            values,
+            touched,
+            errors,
+            handleSubmit,
+            isSubmitting,
+            setFieldValue,
+            setFieldTouched
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <Selector
+                fieldName="establishment_id"
+                fieldValue={values.establishment_id}
+                labelName="Establecimiento"
+                onChange={setFieldValue}
+                onBlur={setFieldTouched}
+                touched={touched.establishment_id}
+                data={getEstablishments}
+                errors={errors.establishment_id}
+              />
+              <p className="label">Fecha</p>
+              <div className="fecha">
+                <Datepicker
+                  placeholder="Desde"
+                  selected={values.desde}
+                  dateFormat="MMMM d, yyyy"
+                  className="form-control"
+                  name="desde"
+                />
+                <Datepicker
+                  selected={values.hasta}
+                  dateFormat="MMMM d, yyyy"
+                  className="form-control"
+                  name="hasta"
+                  placeholder="Hasta"
+                />
+              </div>
+              {/* {errors.password && touched.password && errors.password} */}
+              <br />
+              <button
+                className="btn btn-outline-primary"
+                type="submit"
+                disabled={isSubmitting}
+              >
+                Buscar
+              </button>
+            </form>
+          )}
+        </Formik>
+        <br />
+        <AnimalEstablishmentRegistryTable
+          data={data}
+          toggleModal={toggleModal}
+          setModalRegistryId={setModalRegistryId}
+        />
+        {modalIsOpened && (
+          <Modal>
+            <AnimalEstablishmentRecordDetails registryId={modalRegistryId} />
+            <h2>{modalRegistryId}</h2>
+          </Modal>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SearchAnimalDownDIIO;
--- src/routes/AnimalAdministrationMenuTabs/AnimalDeathRegistrationTab.js
+++ src/routes/AnimalAdministrationMenuTabs/AnimalDeathRegistrationTab.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import PucharseListDiio from "../../components/Diio/PucharseListDiio";
+import SearchAnimalDownDIIO from "../../components/AnimalAdministration/SearchAnimalDownDIIO";
 import axios from "axios";
 import { Container } from "react-bootstrap";
 import { Link } from "@reach/router";
 
-const InventoryDiioTab = () => {
+const AnimalDeathRegistrationTab = () => {
   const [data, setData] = useState([]);
 
   const backendUrl = "http://sipec-backend.herokuapp.com";
@@ -17,46 +17,76 @@ const InventoryDiioTab = () => {
   //     }
   //   }
 
-  async function getDiioPurchases() {
-    var diios = [];
-    diios = await axios.get(`${backendUrl}/diio_stock_table`);
+  // async function getDiioPurchases() {
+  //   var diios = [];
+  //   diios = await axios.get(`${backendUrl}/diio_stock_table`);
 
-    setData(
-      diios.data.map(
-        ({
-          Nregister,
-          vendor: { name: vendor_name, created_at: date },
-          buyer: { name: buyer_name, rup }
-        }) => ({
-          Nregister,
-          vendor_name,
-          buyer_name,
-          date,
-          rup
-        })
-      )
-    );
-  }
+  //   setData(
+  //     diios.data.map(
+  //       ({
+  //         Nregister,
+  //         vendor: { name: vendor_name, created_at: date },
+  //         buyer: { name: buyer_name, rup }
+  //       }) => ({
+  //         Nregister,
+  //         vendor_name,
+  //         buyer_name,
+  //         date,
+  //         rup
+  //       })
+  //     )
+  //   );
+  // }
+
+  // async function getDownDIIOs() {
+  //   var diios = [];
+  //   // diios = await axios.get(`${backendUrl}/diio_stock_table`);
+  //   diios = await axios.get(`${backendUrl}/diio_down_table`);
+
+  //   setData(
+  //     diios.data.map(
+  //       ({
+  //         Nregister,
+  //         vendor: { name: vendor_name, created_at: date },
+  //         buyer: { name: buyer_name, rup }
+  //       }) => ({
+  //         Nregister,
+  //         vendor_name,
+  //         buyer_name,
+  //         date,
+  //         rup
+  //       })
+  //     )
+  //   );
+  // }
 
   useEffect(() => {
-    getDiioPurchases();
+    // getDownDIIOs();
   }, []);
   return (
     <Container>
-      <Link to="nueva" className="btn btn-primary">
-        Nuevo
-      </Link>
-      <PucharseListDiio
-        headers={["Registro", "Comprador", "Fecha", "Nombre"]}
-        data={data.map(purchase => [
-          purchase.Nregister,
-          purchase.vendor_name,
-          purchase.buyer_name,
-          purchase.date,
-          purchase.rup
-        ])}
+      <SearchAnimalDownDIIO
+        headers={[
+          "Fecha Registro",
+          "RUP",
+          "Establecimiento",
+          "RUT",
+          "MVA",
+          "Cantidad"
+        ]}
+        // data={data.map(purchase => [
+        //   purchase.DateRegister,
+        //   purchase.Establishment,
+        //   purchase.RUT,
+        //   purchase.date,
+        //   purchase.rup
+        // ])}
       />
+
+      <Link to="nueva" className="btn btn-primary">
+        Nueva Muerte
+      </Link>
     </Container>
   );
 };
-export default InventoryDiioTab;
+export default AnimalDeathRegistrationTab;
